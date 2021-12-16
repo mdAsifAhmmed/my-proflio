f879b4db88593388db71bfa4780f2702aaf876da
mdAsifAhmmed <mdasifahmed313@gmail.com>
2021-12-17 00:24:41 +0600

    delete some file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,8 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
-import Hero from '../components/Hero/Hero';
-import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
-import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
-
 const Home = () => {
   return (
-    <Layout>
-      <Section grid>
-        <Hero />
-        <BgAnimation />
-      </Section>
-      <Timeline />
-      <Projects />
-      <Technologies />
-
-      {/* <Acomplishments /> */}
-    </Layout>
+    <div>
+      <h1>hello</h1>
+    </div>
   );
 };
 
